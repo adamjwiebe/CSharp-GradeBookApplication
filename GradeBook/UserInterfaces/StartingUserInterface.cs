2c8386da77b30060003501fecea1ad2aab1c6992
Adam <50425202+adamjwiebe@users.noreply.github.com>
2019-11-22 11:51:05 -0600

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,10 +40,9 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2];
+            var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
 
-            //BaseGradeBook gradeBook = new BaseGradeBook(name);
             if (type == "standard")
             {
                 gradeBook = new StandardGradeBook(name);
